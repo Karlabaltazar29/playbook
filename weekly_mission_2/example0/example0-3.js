7abbb6a6d071fd9c0278383a07816a8d436c33b6
karla baltazar <baltazarkarla9@gmail.com>
2022-04-17 20:46:01 -0500

    examples 0 realizados

---
+++ weekly_mission_2/example0/example0-3.js
@@ -0,0 +1,19 @@
+// Ejemplo 3: Objeto con diferentes propiedades
+const myObject3 = {
+    name: "Rodrigo",
+    age: 20,
+    nicknames: [
+     "Tulipan",
+      "Tulancingo",
+      "Tulish"
+    ],
+    address: {
+      zip_code: "90000",
+      street: "Dr. Vertiz 11 Benito Juarez",
+      city: "GUADALAJARA"
+    }
+  }
+  console.log("Ejemplo 3: Objeto con diferentes propiedades")
+  console.log(myObject3)
+  console.log(myObject3.name)
+  console.log(myObject3["address"])
